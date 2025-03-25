7c1a534dd17304ffdc80b02458a571fd77eb4200
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-03-25 17:23:18 +0800

    assign todos

---
+++ main.py
@@ -0,0 +1,9 @@
+#TODO (Kyle Efondo): Get product name, price & quantity 
+ 
+#TODO (Jakim Lopez): Get name & senior id
+
+#TODO (Hanz Gagtan): Compute grand total & apply discount if senior ID is given
+
+#TODO (Pearl Franco): Display inputs & total, apply discount if senior  
+
+#TODO (Miko Causon): Call functions in order & manage program flow
